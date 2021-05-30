5cf1ecb601e6da01da5451597d56239b502a162a
Marco <antnyhern@gmail.com>
2021-05-29 23:29:16 -0500

    All complete project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ColeccionQueu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue MiQ = new Queue();
+
+            MiQ.Enqueue("Hola");
+            MiQ.Enqueue("Mundo");
+            MiQ.Enqueue("Marco");
+
+            foreach (string Fruta in MiQ)
+            {
+                Console.WriteLine("->{0}", Fruta);
+            }
+            Console.WriteLine("------");
+
+            MiQ.Dequeue();
+
+            MiQ.Enqueue("Limon");
+            MiQ.Enqueue("Pera");
+            MiQ.Enqueue("Manzana");
+
+            Console.WriteLine("Peek {0}", MiQ.Peek());
+            Console.WriteLine("Cantidad {0}", MiQ.Count);
+            Console.ReadKey();
+        }
+    }
+}
